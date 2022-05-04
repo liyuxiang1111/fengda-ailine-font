09c9e52b028ce119252c0313c14084fb4f4ce338
liyuxiang1111 <739559572@qq.com>
2022-05-05 00:48:31 +0800

    404页面

--- src/components/Common/404.vue
+++ src/components/Common/404.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="error-container">
+  <div class="error-container w">
     <img src="@/assets/image/sky/3.png" alt="" />
-    <h2>404 not found</h2>
+    <h2>你的页面貌似走丢了</h2>
   </div>
 </template>
 
@@ -9,4 +9,23 @@
 export default {}
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.error-container {
+  position: relative;
+  text-align: center;
+  width: 100%;
+  margin-bottom: -60px;
+  img {
+    width: 100%;
+    height: 70vh;
+  }
+  h2 {
+    position: absolute;
+    left: calc(50% - 110px); /*calc(50% - 盒子宽度的一半)*/
+    top: calc(50% - 25px);
+    width: 220px;
+    height: 50px;
+    color: #ffa500;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,6 @@
   <div class="home-container">
     <!-- 首页导航栏 -->
     <Nav></Nav>
-
     <router-view></router-view>
     <div class="footer-box">
       <Footer></Footer>
@@ -29,6 +28,9 @@ export default {
   height: 100%;
   overflow: auto;
   background-color: #ecf2f7;
+  /* .content {
+    margin-bottom: 60px;
+  } */
   .footer-box {
     margin-top: 60px;
   }
